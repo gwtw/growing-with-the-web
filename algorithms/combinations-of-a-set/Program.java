017454ee47e0b206fea0655074857455a6622e8b
Daniel Imms <tyriar@tyriar.com>
2013-07-25 14:18:26 +1000

    Create Program.java

---
+++ algorithms/combinations-of-a-set/Program.java
@@ -0,0 +1,29 @@
+public class Program {
+    public static void main(String[] args) {
+        ArrayList<string> combinations = getCombinations("abc");
+        
+        assert permutations.size() == 7;
+        assert permutations.contains("a");
+        assert permutations.contains("b");
+        assert permutations.contains("c");
+        assert permutations.contains("ab");
+        assert permutations.contains("ac");
+        assert permutations.contains("bc");
+        assert permutations.contains("abc");
+
+  	    System.out.println("Tests passed");
+    }
+    
+    public ArrayList<String> getCombinations(String text) {
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 0; i < text.length(); i++) {
+            // Record size as we will be adding to the list
+            int resultsLength = results.size();
+            for (int j = 0; j < resultsLength; j++) {
+                results.add(text.charAt(i) + results.get(j));
+            }
+            results.add(Character.toString(text.charAt(i)));
+        }
+        return results;
+    }
+}
