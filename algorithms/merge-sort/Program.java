4fa9f3d53aebdf11f28dcdb7fc8d0a2cf2c340d1
Daniel Imms <tyriar@tyriar.com>
2013-12-07 20:55:58 -0800

    Add natural merge sort, clean other merge sorts

--- algorithms/merge-sort/Program.java
+++ algorithms/merge-sort/Program.java
@@ -1,41 +1,38 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Program {
 	public static void main(String[] args) {
 		int[] sorted = { 1, 2, 4, 6, 9, 9 };
-		
+
 		int[] array = { 9, 6, 2, 9, 1, 4 };
 		int[] mergedArray = mergeSort(array);
-		
+
 		for (int i = 0; i < mergedArray.length; i++) {
 			assert mergedArray[i] == sorted[i];
 		}
-		
+
 		System.out.println("Tests passed");
 	}
-	
+
 	public static int[] mergeSort(int[] array) {
 	    if (array.length <= 1)
 	        return array;
-	
+
 	    int middle = array.length / 2;
 	    int[] left = new int[middle];
 	    int[] right = new int[middle + array.length % 2];
-	    
+
 	    for (int i = 0; i < left.length; i++) {
 	    	left[i] = array[i];
 	    }
 	    for (int i = 0; i < right.length; i++) {
 	    	right[i] = array[i + left.length];
 	    }
-	    
+
 	    left = mergeSort(left);
 	    right = mergeSort(right);
-	
+
 	    return merge(left, right);
 	}
-	
+
 	public static int[] merge(int[] left, int[] right) {
 		int[] result = new int[left.length + right.length];
 		int leftIndex = 0;
