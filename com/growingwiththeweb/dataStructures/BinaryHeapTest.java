61ae66f248801bb604a4ddff011a445de981cc9d
Daniel Imms <tyriar@tyriar.com>
2014-08-19 02:16:11 -0700

    Add new test

--- com/growingwiththeweb/dataStructures/BinaryHeapTest.java
+++ com/growingwiththeweb/dataStructures/BinaryHeapTest.java
@@ -2,6 +2,7 @@ package com.growingwiththeweb.dataStructures;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 import org.junit.Ignore;
@@ -18,13 +19,28 @@ public class BinaryHeapTest {
         heap.insert(3);
         heap.insert(4);
         heap.insert(5);
-        assertEquals("size should be 5", heap.size(), 5);
+        assertEquals(heap.size(), 5);
     }
 
     @Test
     public void testExtractMinimumWithEmptyHeap() {
         BinaryHeap<Integer> heap = new BinaryHeap<Integer>();
-        assertEquals("should be null", heap.extractMin(), null);
+        assertEquals(heap.extractMin(), null);
+    }
+
+    @Test
+    public void testExtractMinimumItems() {
+        BinaryHeap<Integer> heap = new BinaryHeap<Integer>();
+        heap.insert(5);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(1);
+        heap.insert(2);
+        assertSame(heap.extractMin(), 1);
+        assertSame(heap.extractMin(), 2);
+        assertSame(heap.extractMin(), 3);
+        assertSame(heap.extractMin(), 4);
+        assertSame(heap.extractMin(), 5);
     }
 
 }
