e98d89277eec367f8c6f3bb51950adc23e340b6f
Daniel Imms <tyriar@tyriar.com>
2015-06-14 17:25:46 -0700

    Add more heap tests
    Fixes #26

--- test/com/growingwiththeweb/datastructures/BaseHeapTest.java
+++ test/com/growingwiththeweb/datastructures/BaseHeapTest.java
@@ -1,6 +1,7 @@
 package com.growingwiththeweb.datastructures;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertSame;
 
@@ -49,5 +50,52 @@ public abstract class BaseHeapTest {
         assertSame(heap.extractMin(), 3);
         assertSame(heap.extractMin(), 4);
         assertSame(heap.extractMin(), 5);
+    }
+
+    @Test
+    public void testExtractMinimumFromJumbledHeap() {
+        heap.insert(1);
+        heap.insert(4);
+        heap.insert(3);
+        heap.insert(5);
+        heap.insert(2);
+        assertSame(heap.extractMin(), 1);
+        assertSame(heap.extractMin(), 2);
+        assertSame(heap.extractMin(), 3);
+        assertSame(heap.extractMin(), 4);
+        assertSame(heap.extractMin(), 5);
+    }
+
+    @Test
+    public void testExtractMinimumWithNegativeItems() {
+        heap.insert(-9);
+        heap.insert(6);
+        heap.insert(3);
+        heap.insert(10);
+        heap.insert(-4);
+        assertSame(heap.extractMin(), -9);
+        assertSame(heap.extractMin(), -4);
+        assertSame(heap.extractMin(), 3);
+        assertSame(heap.extractMin(), 6);
+        assertSame(heap.extractMin(), 10);
+    }
+
+    @Test
+    public void testFindMinimum() {
+        heap.insert(5);
+        heap.insert(3);
+        heap.insert(1);
+        heap.insert(4);
+        heap.insert(2);
+        assertSame(heap.min(), 1);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(heap.isEmpty());
+        heap.insert(1);
+        assertFalse(heap.isEmpty());
+        heap.extractMin();
+        assertTrue(heap.isEmpty());
     }
 }
