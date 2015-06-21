13fcfe538becc6a0e72d2c25a26e42f3f395c52c
Daniel Imms <tyriar@tyriar.com>
2015-06-20 22:56:53 -0700

    Add exception for division by zero

--- src/com/growingwiththeweb/algorithms/interviewquestions/dividewithoutdivide/Program.java
+++ src/com/growingwiththeweb/algorithms/interviewquestions/dividewithoutdivide/Program.java
@@ -1,17 +1,31 @@
 package com.growingwiththeweb.algorithms.interviewquestions.dividewithoutdivide;
 
+import java.lang.ArithmeticException;
+
 public class Program {
     public static void main(String[] args) {
-        assert divide(10,4) == 2;
-        assert divide(11,4) == 2;
-        assert divide(12,4) == 3;
-        assert divide(13,4) == 3;
-        assert divide(130,3) == 43;
+        assert divide(10, 4) == 2;
+        assert divide(11, 4) == 2;
+        assert divide(12, 4) == 3;
+        assert divide(13, 4) == 3;
+        assert divide(130,3 ) == 43;
+
+        boolean threw = false;
+        try {
+            divide(1, 0);
+        } catch (ArithmeticException ex) {
+            threw = true;
+        }
+        assert threw;
 
         System.out.println("Tests passed");
     }
 
     public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException(
+                    "Division by 0 is undefined: " + a + "/" + b);
+        }
         int sign = 1;
         if (a < 0) {
             a = -a;
