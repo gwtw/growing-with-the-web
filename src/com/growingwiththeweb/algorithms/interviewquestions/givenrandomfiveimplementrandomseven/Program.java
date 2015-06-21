76d44ad6663eb4e49fc87debd672d8837b2c2476
Daniel Imms <tyriar@tyriar.com>
2015-06-21 14:31:13 -0700

    Change random7 result order

--- src/com/growingwiththeweb/algorithms/interviewquestions/givenrandomfiveimplementrandomseven/Program.java
+++ src/com/growingwiththeweb/algorithms/interviewquestions/givenrandomfiveimplementrandomseven/Program.java
@@ -11,10 +11,10 @@ public class Program {
     }
 
     public static int random7() {
-        // Get 0, 5, 10, 15 or 20 then add 1-5 (4% chance for 1-25)
-        int val = (random5() - 1) * 5 + random5();
-        // If 1-21, return the result modulo 7 (12% chance for 1-7), otherwise call
-        // recursively (16% chance)
-        return val > 21 ? random7() : val % 7 + 1;
+        // Get 0, 5, 10, 15 or 20 then add 0-4 (4% chance for 0-24)
+        int val = (random5() - 1) * 5 + (random5() - 1);
+        // If 0-20, return the result modulo 7 + 1 (12% chance for 1-7), otherwise
+        // call recursively (16% chance)
+        return val >= 21 ? random7() : val % 7 + 1;
     }
 }
