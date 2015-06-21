faf1ac145bc63f8eb574a09fd34d14d2e433e674
Daniel Imms <tyriar@tyriar.com>
2015-06-20 19:17:21 -0700

    Add StackQueue, seemed to have not been committed

---
+++ src/com/growingwiththeweb/algorithms/interviewquestions/twostackqueue/StackQueue.java
@@ -0,0 +1,22 @@
+package com.growingwiththeweb.algorithms.interviewquestions.twostackqueue;
+
+public class StackQueue<T> {
+    Stack<T> s1 = new Stack<T>();
+    Stack<T> s2 = new Stack<T>();
+
+    void push(T obj) {
+        s1.push(obj);
+    }
+
+    T pop() {
+        if (s2.isEmpty()) {
+            if (s1.isEmpty()) {
+                return null;
+            }
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+}
