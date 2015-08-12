cb9fc0246ac03209ca7fe0378ddac425c33b6199
Daniel Imms <tyriar@tyriar.com>
2015-08-11 23:24:48 -0700

    Move regression test case to BST test only

--- test/com/growingwiththeweb/datastructures/BinarySearchTreeTest.java
+++ test/com/growingwiththeweb/datastructures/BinarySearchTreeTest.java
@@ -1,6 +1,7 @@
 package com.growingwiththeweb.datastructures;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertSame;
 
@@ -15,5 +16,31 @@ public class BinarySearchTreeTest extends BaseBinarySearchTreeTest {
     @Override
     public void setUp() {
         tree = new BinarySearchTree<Integer>();
+    }
+
+    /**
+     * delete(3), 5 should remain in the tree, there should not be duplicate
+     * nodes for 4.
+     * 1
+     *  \
+     *   3
+     *  / \
+     * 2   4
+     *      \
+     *       5
+     */
+    @Test
+    public void testDeleteRegressionCaseOne() {
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(5);
+        String treeBefore = tree.toString();
+        assertEquals(5, tree.size());
+        tree.delete(3);
+        String treeAfter = tree.toString();
+        assertNotSame(treeBefore.length(), treeAfter.length());
+        System.out.println("After delete: " + tree.toString());
     }
 }
