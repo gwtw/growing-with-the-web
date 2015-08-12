cad52304cf5f880e34a4b6978066fbfb0a5c0701
Daniel Imms <tyriar@tyriar.com>
2015-08-11 23:06:21 -0700

    Regression test case for BinarySearchTree.delete bug
    #76

--- test/com/growingwiththeweb/datastructures/BaseBinarySearchTreeTest.java
+++ test/com/growingwiththeweb/datastructures/BaseBinarySearchTreeTest.java
@@ -2,6 +2,7 @@ package com.growingwiththeweb.datastructures;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertSame;
 
@@ -169,6 +170,32 @@ public abstract class BaseBinarySearchTreeTest {
         assertEquals(7, tree.size());
         tree.delete(50);
         assertEquals(7, tree.size());
+    }
+
+    /**
+     * delete(3), 5 should remain in the tree, there should not be duplicate
+     * nodes for 4.
+     * 1
+     *  \
+     *   3
+     *  / \
+     * 2   4
+     *      \
+     *       5
+     */
+    @Test
+    public void testDeleteRegressionCaseOne() {
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(5);
+        String treeBefore = tree.toString();
+        assertEquals(5, tree.size());
+        tree.delete(3);
+        String treeAfter = tree.toString();
+        assertNotSame(treeBefore.length(), treeAfter.length());
+        System.out.println("After delete: " + tree.toString());
     }
 
     @Test
