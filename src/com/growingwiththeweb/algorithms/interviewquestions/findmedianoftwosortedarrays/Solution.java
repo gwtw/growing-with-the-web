9a5aa0b9923160b40ff9555032aeff0840ea61bc
Daniel Imms <tyriar@tyriar.com>
2015-08-23 18:22:05 -0700

    Add find median of two sorted arrays

---
+++ src/com/growingwiththeweb/algorithms/interviewquestions/findmedianoftwosortedarrays/Solution.java
@@ -0,0 +1,164 @@
+package com.growingwiththeweb.algorithms.interviewquestions.findmedianoftwosortedarrays;
+
+import java.lang.IllegalArgumentException;
+
+public class Solution {
+    /**
+     * Gets the median of two sorted arrays.
+     *
+     * @param A The first sorted array.
+     * @param B The second sorted array.
+     * @return The median of A and B.
+     */
+    public static double findMedian(int[] A, int[] B) {
+        return findMedianInternal(A, 0, A.length - 1, B, 0, B.length - 1);
+    }
+
+    /**
+     * Gets the median of two sorted arrays.
+     *
+     * @param A The first sorted array.
+     * @param aStart The start index of A to use.
+     * @param aEnd The end index of A to use.
+     * @param B The second sorted array.
+     * @param bStart The start index of B to use.
+     * @param bEnd The end index of B to use.
+     * @return The median of A and B.
+     */
+    private static double findMedianInternal(
+            int[] A, int aStart, int aEnd, int[] B, int bStart, int bEnd) {
+        // Check actual lengths here instead of start/end since the arrays
+        // themselves don't change in recursive calls
+        if (A.length == 0 && B.length == 0) {
+            throw new IllegalArgumentException(
+                    "Both arrays cannot be zero-length");
+        }
+        if (A.length == 0) {
+            return medianOfArray(B, bStart, bEnd);
+        }
+        if (B.length == 0) {
+            return medianOfArray(A, aStart, aEnd);
+        }
+
+        int aLength = (aEnd - aStart) + 1;
+        int bLength = (bEnd - bStart) + 1;
+        if (aLength == 1 && bLength == 1) {
+            return (A[aStart] + B[bStart]) / 2d;
+        }
+        if (aLength == 1 && bLength > 1) {
+            return findMedianOfArrayAndValue(B, bStart, bEnd, A[aStart]);
+        }
+        if (bLength == 1 && aLength > 1) {
+            return findMedianOfArrayAndValue(A, aStart, aEnd, B[bStart]);
+        }
+
+        double medianA = medianOfArray(A, aStart, aEnd);
+        double medianB = medianOfArray(B, bStart, bEnd);
+        if (medianA == medianB) {
+            return medianA;
+        }
+        int maxDiscardable = Math.min((aEnd - aStart) / 2 - 1,
+                                      (bEnd - bStart) / 2 - 1);
+        if (maxDiscardable < 1) {
+            maxDiscardable = 1;
+        }
+        if (medianA < medianB) {
+            return findMedianInternal(A, aStart + maxDiscardable, aEnd,
+                                      B, bStart, bEnd - maxDiscardable);
+        }
+        return findMedianInternal(A, aStart, aEnd - maxDiscardable,
+                                  B, bStart + maxDiscardable, bEnd);
+    }
+
+    /**
+     * Gets the median of a sorted array and an additional value.
+     *
+     * @param array The sorted array.
+     * @param start The start of the array to use.
+     * @param end The end of the array to use.
+     * @param value The additional value to include in median calculation.
+     * @return The median of the array including the additional value.
+     */
+    private static double findMedianOfArrayAndValue(
+            int[] array, int start, int end, int value) {
+        double arrayMedian = medianOfArray(array, start, end);
+        if (arrayMedian == value) {
+            return arrayMedian;
+        }
+        // If array[start..end].length is even
+        if ((end - start) % 2 == 1) {
+            return findMedianOfArrayAndValueEvenCase(array, start, end, arrayMedian, value);
+        }
+        return findMedianOfArrayAndValueOddCase(array, start, end, arrayMedian, value);
+    }
+
+    /**
+     * Gets the median of a sorted array with an odd number of elements plus an
+     * additional value.
+     *
+     * @param array The sorted array.
+     * @param start The start of the array to use.
+     * @param end The end of the array to use.
+     * @param arrayMedian The median of the array excluding the additional
+     * value.
+     * @param value The additional value to include in median calculation.
+     * @return The median of the array including the additional value.
+     */
+    private static double findMedianOfArrayAndValueOddCase(
+            int[] array, int start, int end, double arrayMedian, int value) {
+        int midIndex = (end + start) / 2;
+        int left = array[midIndex - 1];
+        int mid = array[midIndex];
+        int right = array[midIndex + 1];
+        if (value < left) {
+            return (mid + left) / 2d;
+        }
+        if (value > right) {
+            return (mid + right) / 2d;
+        }
+        return (mid + value) / 2d;
+    }
+
+    /**
+     * Gets the median of a sorted array with an even number of elements plus an
+     * additional value.
+     *
+     * @param array The sorted array.
+     * @param start The start of the array to use.
+     * @param end The end of the array to use.
+     * @param arrayMedian The median of the array excluding the additional
+     * value.
+     * @param value The additional value to include in median calculation.
+     * @return The median of the array including the additional value.
+     */
+    private static double findMedianOfArrayAndValueEvenCase(
+            int[] array, int start, int end, double arrayMedian, int value) {
+        if (arrayMedian > value) {
+            int left = array[(end - start) / 2];
+            return Math.max(left, value);
+        } else {
+            int right = array[(end - start) / 2 + 1];
+            return Math.min(right, value);
+        }
+    }
+
+    /**
+     * Gets the median of a single sorted array.
+     *
+     * @param array The sorted array to get the median of.
+     * @param start The start of the array to use.
+     * @param end The end of the array to use.
+     * @return The median of the array.
+     */
+    private static double medianOfArray(int[] array, int start, int end) {
+        if (start == end) {
+            return array[start];
+        }
+        int mid = (start + end) / 2;
+        // If array[start..end].length is even
+        if ((end - start) % 2 == 1) {
+            return (array[mid] + array[mid + 1]) / 2d;
+        }
+        return array[mid];
+    }
+}
---
+++ test/com/growingwiththeweb/algorithms/interviewquestions/findmedianoftwosortedarrays/SolutionTest.java
@@ -0,0 +1,134 @@
+package com.growingwiththeweb.algorithms.interviewquestions.findmedianoftwosortedarrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.IllegalArgumentException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test(expected=IllegalArgumentException.class)
+    public void testTwoEmptyArrays() {
+        Solution.findMedian(new int[] {}, new int[] {});
+    }
+
+    @Test
+    public void testSingleElementArray() {
+        assertEquals(1d, Solution.findMedian(new int[] {}, new int[] {1}), 0d);
+        assertEquals(1d, Solution.findMedian(new int[] {1}, new int[] {}), 0d);
+    }
+
+    @Test
+    public void testTwoSingleElementArrays() {
+        assertEquals(2d, Solution.findMedian(new int[] {1}, new int[] {3}), 0d);
+    }
+
+    @Test
+    public void testEmptyFirstArray() {
+        assertEquals(3d, Solution.findMedian(new int[] {}, new int[] {1, 2, 3, 4, 5}), 0d);
+        assertEquals(10d, Solution.findMedian(new int[] {}, new int[] {-2, 6, 10, 22, 23}), 0d);
+        assertEquals(4d, Solution.findMedian(new int[] {}, new int[] {1, 2, 4, 4, 5, 6}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {}, new int[] {1, 2, 3, 4, 5, 6}), 0d);
+    }
+
+    @Test
+    public void testEmptySecondArray() {
+        assertEquals(3d, Solution.findMedian(new int[] {1, 2, 3, 4, 5}, new int[] {}), 0d);
+        assertEquals(10d, Solution.findMedian(new int[] {-2, 6, 10, 22, 23}, new int[] {}), 0d);
+        assertEquals(4d, Solution.findMedian(new int[] {1, 2, 4, 4, 5, 6}, new int[] {}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6}, new int[] {}), 0d);
+    }
+
+    @Test
+    public void testTwoSmallArrays() {
+        assertEquals(5d, Solution.findMedian(new int[] {1, 3, 5, 6}, new int[] {3, 5, 6, 8}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {3, 4, 5, 6, 7}, new int[] {4, 5, 6, 7, 8}), 0d);
+        assertEquals(4d, Solution.findMedian(new int[] {1, 2, 3, 4}, new int[] {4, 5, 6, 7}), 0d);
+        assertEquals(4.5d, Solution.findMedian(new int[] {1, 2, 3, 4}, new int[] {5, 6, 7, 8}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {1, 2, 3, 4}, new int[] {6, 7, 8, 9}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {1, 2, 3, 4}, new int[] {7, 8, 9, 10}), 0d);
+        assertEquals(1d, Solution.findMedian(new int[] {1, 1, 1, 1}, new int[] {1, 1, 1, 1}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {1, 2, 3, 4, 5}, new int[] {2, 3, 4, 5, 6}), 0d);
+        assertEquals(2.5d, Solution.findMedian(new int[] {-1, 0, 1, 2, 5}, new int[] {2, 3, 4, 5, 6}), 0d);
+    }
+
+    @Test
+    public void testLargerFirstArray() {
+        assertEquals(5d, Solution.findMedian(new int[] {1, 2, 4, 6, 7}, new int[] {3, 5, 8, 9}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6, 8, 9, 10}, new int[] {7}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6, 8, 9}, new int[] {7}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, new int[] {1, 2, 3}), 0d);
+        assertEquals(3d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6, 7, 8}, new int[] {1, 2, 3}), 0d);
+        assertEquals(4.5d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[] {2, 3}), 0d);
+        assertEquals(4d, Solution.findMedian(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, new int[] {2, 3}), 0d);
+    }
+
+    @Test
+    public void testLargerFirstArrayWhenMedianIsOther() {
+        assertEquals(5d, Solution.findMedian(new int[] {1, 2, 3, 4, 6, 7, 8, 9}, new int[] {5}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {4, 7}, new int[] {5}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {1, 4, 7, 9}, new int[] {5}), 0d);
+    }
+
+    @Test
+    public void testLargerSecondArray() {
+        assertEquals(5d, Solution.findMedian(new int[] {3, 5, 8, 9}, new int[] {1, 2, 4, 6, 7}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {7}, new int[] {1, 2, 3, 4, 5, 6, 8, 9, 10}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {7}, new int[] {1, 2, 3, 4, 5, 6, 8, 9}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {1, 2, 3}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}), 0d);
+        assertEquals(3d, Solution.findMedian(new int[] {1, 2, 3}, new int[] {1, 2, 3, 4, 5, 6, 7, 8}), 0d);
+        assertEquals(4.5d, Solution.findMedian(new int[] {2, 3}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), 0d);
+        assertEquals(4d, Solution.findMedian(new int[] {2, 3}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}), 0d);
+    }
+
+    @Test
+    public void testLargerSecondArrayWhenMedianIsOther() {
+        assertEquals(5d, Solution.findMedian(new int[] {5}, new int[] {1, 2, 3, 4, 6, 7, 8, 9}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {5}, new int[] {4, 7}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {5}, new int[] {1, 4, 7, 9}), 0d);
+    }
+
+    @Test
+    public void testArrayAndValueFallBackLeftLeftCase() {
+        assertEquals(2.5d, Solution.findMedian(new int[] {-1}, new int[] {1, 4, 7}), 0d);
+        assertEquals(2.5d, Solution.findMedian(new int[] {0}, new int[] {1, 4, 7}), 0d);
+        assertEquals(2.5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {-1}), 0d);
+        assertEquals(2.5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {0}), 0d);
+    }
+
+    @Test
+    public void testArrayAndValueFallBackLeftCase() {
+        assertEquals(3d, Solution.findMedian(new int[] {2}, new int[] {1, 4, 7}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {3}, new int[] {1, 4, 7}), 0d);
+        assertEquals(3d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {2}), 0d);
+        assertEquals(3.5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {3}), 0d);
+    }
+
+    @Test
+    public void testArrayAndValueFallBackMidCase() {
+        assertEquals(4d, Solution.findMedian(new int[] {4}, new int[] {1, 4, 7}), 0d);
+        assertEquals(4d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {4}), 0d);
+    }
+
+    @Test
+    public void testArrayAndValueFallBackRightCase() {
+        assertEquals(4.5d, Solution.findMedian(new int[] {5}, new int[] {1, 4, 7}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {6}, new int[] {1, 4, 7}), 0d);
+        assertEquals(4.5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {5}), 0d);
+        assertEquals(5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {6}), 0d);
+    }
+
+    @Test
+    public void testArrayAndValueFallBackRightRightCase() {
+        assertEquals(5.5d, Solution.findMedian(new int[] {8}, new int[] {1, 4, 7}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {9}, new int[] {1, 4, 7}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {8}), 0d);
+        assertEquals(5.5d, Solution.findMedian(new int[] {1, 4, 7}, new int[] {9}), 0d);
+    }
+}
