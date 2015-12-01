dda2fe65af95af505797adf3ba10b0e4a2e202d4
Daniel Imms <tyriar@tyriar.com>
2015-12-01 11:08:18 +1100

    Add visibility modifiers to StackQueue members

--- src/com/growingwiththeweb/algorithms/interviewquestions/twostackqueue/StackQueue.java
+++ src/com/growingwiththeweb/algorithms/interviewquestions/twostackqueue/StackQueue.java
@@ -3,14 +3,14 @@ package com.growingwiththeweb.algorithms.interviewquestions.twostackqueue;
 import java.util.Stack;
 
 public class StackQueue<T> {
-    Stack<T> s1 = new Stack<T>();
-    Stack<T> s2 = new Stack<T>();
+    private Stack<T> s1 = new Stack<T>();
+    private Stack<T> s2 = new Stack<T>();
 
-    void push(T obj) {
+    public void push(T obj) {
         s1.push(obj);
     }
 
-    T pop() {
+    public T pop() {
         if (s2.isEmpty()) {
             if (s1.isEmpty()) {
                 return null;
